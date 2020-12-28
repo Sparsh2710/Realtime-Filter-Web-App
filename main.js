fa11ad3fbee132c1cfac70d34f679a3df7cf3839
Sparsh2710 <70764422+Sparsh2710@users.noreply.github.com>
2020-12-28 11:21:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
-        noseX = results[0].pose.nose.x - 10;
+        noseX = results[0].pose.nose.x - 15;
         noseY = results[0].pose.nose.y - 10;
         console.log("nose x = " + noseX);
         console.log("nose y = " + noseY);
